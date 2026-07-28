b20ccce59694249014eeed596c9c0573d0c9d1ac
Raunak Kumar <162709303+Raunakupadhyay@users.noreply.github.com>
2026-07-28 22:47:51 +0530

    07-28-2026 1011-capacity-to-ship-packages-within-d-days .java Array

---
+++ LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int low = 0, high = 0;
+        for (int w : weights) {
+            low = Math.max(low,w);   
+            high += w;                
+        }
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (canShip(weights, days, mid)) {
+                high = mid; 
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return low;
+    }
+    private boolean canShip(int[] weights, int days, int capacity) {
+        int daysNeeded = 1, currentLoad = 0;
+        for (int w : weights) {
+            if (currentLoad + w > capacity) {
+                daysNeeded++;
+                currentLoad = 0;
+            }
+            currentLoad += w;
+        }
+        return daysNeeded <= days;
+    }
+}
